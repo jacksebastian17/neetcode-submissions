4cc80c760cb9b6141e5432aab3a42b3f7d4d0c31
Jack Sebastian <55264197+jacksebastian17@users.noreply.github.com>
2026-07-21 00:56:45 -0500

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = {}
+        for s in strs: # s: act
+            count = [0] * 26
+            for c in s: # a
+                index = ord(c) - ord("a")
+                count[index] += 1
+            # [1, 0, 1, ..., 1, ..., 0]
+            #  a     c       t
+            key = tuple(count) # (1, 0, 1, ..., 1, ..., 0)
+
+            if key not in groups:
+                groups[key] = []
+            groups[key].append(s)
+        
+        return list(groups.values())
