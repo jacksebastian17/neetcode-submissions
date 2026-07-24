88a75d78e72300c827c903dffd360af28173fe9c
Jack Sebastian <55264197+jacksebastian17@users.noreply.github.com>
2026-07-24 04:19:24 -0500

    Add: non-cyclical-number - submission-5

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-5.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+        currNum = self.sumOfDigits(n)
+        seen.add(currNum)
+        while currNum != 1:
+            currNum = self.sumOfDigits(currNum)
+            if currNum in seen:
+                return False
+            seen.add(currNum)
+        return True
+
+        return True
+    def sumOfDigits(self, n: int) -> int:
+        digits = []
+        while n > 0:
+            digit = n % 10  
+            n = n // 10
+            digits.append(digit)
+        return sum(x**2 for x in digits)
+
