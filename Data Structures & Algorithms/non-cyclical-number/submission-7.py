5396d8a6319e26f65be86363156e3e5566125950
Jack Sebastian <55264197+jacksebastian17@users.noreply.github.com>
2026-07-24 04:26:41 -0500

    Add: non-cyclical-number - submission-7

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-7.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        slow = self.sumOfDigits(n)
+        fast = self.sumOfDigits(self.sumOfDigits(n))
+        while slow != 1:
+            if slow == fast:
+                return False
+            slow = self.sumOfDigits(slow)
+            fast = self.sumOfDigits(self.sumOfDigits(fast))
+        return True
+
+    def sumOfDigits(self, n: int) -> int:
+        total = 0
+        while n > 0:
+            digit = n % 10
+            total += digit**2
+            n //= 10
+        return total
