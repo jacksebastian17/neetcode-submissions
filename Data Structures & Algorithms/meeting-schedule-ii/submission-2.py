b41096920b20f4db2d2bce762cc7b113718469d5
Jack Sebastian <55264197+jacksebastian17@users.noreply.github.com>
2026-07-30 04:25:20 -0500

    Add: meeting-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-2.py
@@ -0,0 +1,25 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        events = []
+        for m in intervals:
+            events.append((m.start, +1))
+            events.append((m.end, -1))
+        print(events)
+
+        events.sort(key=lambda x: (x[0], x[1]))
+
+        active = 0
+        max_rooms = 0
+        for (eventTime, roomDelta) in events:
+            active += roomDelta
+            max_rooms = max(max_rooms, active)
+
+        return max_rooms
